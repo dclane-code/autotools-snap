0aa3e9ca7b989be9341485a1ffab1c3378fba860
David Lane <david.lane@canonical.com>
2023-02-13 12:03:01 +1100

    the simplest program possible

---
+++ Makefile
@@ -0,0 +1,5 @@
+dclane-hello: main.c
+	gcc -o dclane-hi main.c
+
+clean:
+	rm -rf dclane-hi
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+    printf("dave's first snap from github\n");
+    return 0;
+
+}
